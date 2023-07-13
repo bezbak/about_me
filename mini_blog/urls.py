e4a86070d5e5a207a9e800b94cb7d47c6c0d92f9
itek <itybek@gmail.com>
2023-07-13 14:54:56 +0600

    add url and static

--- mini_blog/urls.py
+++ mini_blog/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from django.urls import path
-from me.views import index
-
+# from django.conf.urls.static import static
+# from django.conf import settings
+from me.views import index,second
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index,name='index')
+    path('', index ,name='1'),
+    path('second/', second ,name='2'),
 ]
+# urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
