d1374508e491100793cae1ba35a8c5e3e9345ddc
itek <itybek@gmail.com>
2023-07-15 14:17:39 +0600

    view me

--- me/views.py
+++ me/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
+from me.models import Me
 # Create your views here.
 def index(request):
-    return render(request, 'index.html')
+    me = Me.objects.all()
+    context = {
+        'people':me
+    }
+    return render(request, 'index.html', context)
 
 def second(request):
     return render(request, 'second.html')
